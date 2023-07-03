871be4f77f37c5235901859ec4fe18c2581a3471
SirPhoros <cgtorrubia@gmail.com>
2023-07-03 17:11:56 +0100

    implemente calculator functionality

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -16,6 +16,7 @@ export class AppComponent {
   pressNum = (num: string) => {
     // Append the pressed number to the input and calculate the answer
     this.input = this.input + num;
+    this.calcAnswer();
   };
 
   // Method triggered when an operator button is pressed
@@ -33,6 +34,7 @@ export class AppComponent {
 
     // Append the pressed operator to the input and calculate the answer
     this.input = this.input + op;
+    this.calcAnswer();
   }
 
   clear() {
@@ -44,5 +46,60 @@ export class AppComponent {
   allClear() {
     this.result = '';
     this.input = '';
+  }
+
+  // Method to calculate the answer based on the current input
+  calcAnswer() {
+    let formula = this.input;
+
+    let lastKey = formula[formula.length - 1];
+
+    // Remove trailing operator or '.' from the formula if present
+    if (
+      lastKey === '/' ||
+      lastKey === '*' ||
+      lastKey === '-' ||
+      lastKey === '+' ||
+      lastKey === '.'
+    ) {
+      formula = formula.slice(0, formula.length - 1);
+    }
+
+    console.log('Formula ' + formula);
+    this.result = this.evaluateFormula(formula);
+  }
+
+  // Method to evaluate the formula and calculate the result
+  evaluateFormula(formula: string): string {
+    const operators: { [key: string]: (a: number, b: number) => number } = {
+      '+': (a: number, b: number) => a + b,
+      '-': (a: number, b: number) => a - b,
+      '*': (a: number, b: number) => a * b,
+      '/': (a: number, b: number) => a / b,
+    };
+
+    const expression = formula.match(/(\d+|\+|-|\*|\/)/g);
+
+    if (!expression) {
+      return '';
+    }
+
+    let result = Number(expression[0]);
+
+    // Perform the calculations based on the expression
+    for (let i = 1; i < expression.length; i += 2) {
+      const operator = expression[i];
+      const operand = Number(expression[i + 1]);
+
+      // Check if the operator is valid and the operand is a number
+      if (!operators[operator] || isNaN(operand)) {
+        return '';
+      }
+
+      // Apply the operator function to the current result and operand
+      result = operators[operator](result, operand);
+    }
+
+    return result.toString();
   }
 }
