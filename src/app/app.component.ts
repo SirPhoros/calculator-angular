5283530319919e47a97bdcca97cd7d2212b36a83
SirPhoros <cgtorrubia@gmail.com>
2023-07-03 17:03:13 +0100

    Add button function

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,17 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'calculator-angular';
+  title: string = 'calculator-angular';
+
+  // Input and result variables to store the user input and calculated result
+  input: string = '';
+  result: string = '';
+
+  // Method triggered when a number button is pressed
+  pressNum = (num: string) => {
+    console.log(num);
+  };
 }
